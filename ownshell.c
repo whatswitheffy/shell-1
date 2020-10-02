ae59778b6c39cc757b1cd04c95cbef96e9c4f365
LeoRaiderNL <72147933+LeoRaiderNL@users.noreply.github.com>
2020-10-02 09:23:23 +0600

    Version 2

--- ownshell.c
+++ ownshell.c
@@ -6,18 +6,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int strcmp(char *str1, char *str2) {
-    int i = -1;
-    do {
-        i++;
-        if (str1[i] < str2[i])
-            return 1;
-        if (str1[i] > str2[i])
-            return 2;
-    } while (str1[i] && str2[i]);
-    return 0;
-}
-
 char *get_word(char *last_ch) {
     char ch, *word = NULL;
     ch = getchar();
@@ -64,53 +52,15 @@ void free_list(char **list) {
     return;
 }
 
-int iodetector(char **cmd, int *ioflag) {
-    int i = 0, fd = 0, flag = 0;
-    for (i = 0; cmd[i] != NULL; i++) {
-        if (cmd[i][0] == '>') {
-            if (cmd[i + 1] != NULL) {
-                fd = open(cmd[i + 1], O_WRONLY | O_CREAT | O_TRUNC,
-                                  S_IRUSR | S_IWUSR);
-                flag = 1;
-                break;
-            }
-        }
-        if (cmd[i][0] == '<') {
-            if (cmd[i + 1] != NULL) {
-                fd = open(cmd[i], O_RDONLY | O_CREAT | O_TRUNC,
-                                  S_IRUSR | S_IWUSR);
-                flag = 0;
-                break;
-            }
-        }
-    }
-    *ioflag = flag;
-    return fd;
-}
-
-int execution(char **cmd) {
-    pid_t pid;
-    int fd = 0, ioflag = 0;
-    fd = iodetector(cmd, &ioflag);
-    pid = fork();
-    if (ioflag) {
-        dup2(fd, 1);
-    } else {
-        dup2(fd, 0);
-    }
-    if (pid > 0) {
-        wait(NULL);
-    } else {
-        if (ioflag) {
-            dup2(fd, 1);
-        } else {
-            dup2(fd, 0);
-        }
-        if (execvp(cmd[0], cmd) < 0) {
-            perror("exec failed\n");
+int strcmp(char *str1, char *str2) {
+    int i = -1;
+    do {
+        i++;
+        if (str1[i] < str2[i])
             return 1;
-        }
-    }
+        if (str1[i] > str2[i])
+            return 2;
+    } while (str1[i] && str2[i]);
     return 0;
 }
 
