07165d641d308d25c24810c04e1f7171144671e2
LeoRaiderNL <72147933+LeoRaiderNL@users.noreply.github.com>
2020-10-08 20:53:46 +0600

    Version 4
    infinit(), io_file()

--- ownshell.c
+++ ownshell.c
@@ -52,69 +52,59 @@ void delete_list(char **list) {
     return;
 }
 
-int check_io(char **list, int *io) {
-    int flag = 0, fd = 0;
+int io_file(char **list, int fd, pid_t pid) {
     for (int i = 0; list[i] != NULL; i++) {
-        if (list[i][0] == '>') {
+        if (list[0][i] == '>') {
             if (list[i + 1] != NULL) {
-                fd = open(list[i + 1], O_WRONLY | O_CREAT | O_TRUNC,
-                                   S_IRUSR | S_IWUSR);
-                flag = 1;
-                break;
+                fd = open(list[i + 1],
+                            O_WRONLY | O_CREAT | O_TRUNC,
+                            S_IRUSR | S_IWUSR);
+                if (pid == 0) {
+                    dup2(fd, 1);
+                    break;
+                }
             }
         }
-        if (list[i][0] == '<') {
+        if (list[0][i] == '<') {
             if (list[i + 1] != NULL) {
-                fd = open(list[1], O_RDONLY | O_CREAT | O_TRUNC,
-                               S_IRUSR | S_IWUSR);
-                flag = 0;
-                break;
+                fd = open(list[i + 1], 
+                            O_RDONLY | O_CREAT | O_TRUNC,
+                            S_IRUSR | S_IWUSR);
+                if (pid == 0) {
+                    dup2(fd, 0);
+                    break;
+                }
+
             }
         }
     }
-    *io = flag;
     return fd;
 }
 
-
-int perform(char **list) {
+void infinit(char **list) {
     pid_t pid;
-    int fd = 0, io = 0;
-    fd = check_io(list, &io);
-    pid = fork();
-    if (io) {
-        dup2(fd, 1);
-    } else {
-        dup2(fd, 0);
-    }
-    if (pid > 0) {
-        wait(NULL);
-    } else {
-        if (io) {
-            dup2(fd, 1);
+    int fd = 0;
+    while(strcmp(list[0], "exit") && strcmp(list[0], "quit")) {
+        pid = fork();
+        fd = io_file(list, fd, pid);
+        if (pid > 0) {
+            wait(NULL);
         } else {
-            dup2(fd, 0);
-        }
-        if (execvp(list[0], list) < 0) {
-            perror("Execvp failed!\n");
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int main() {
-    char **list = NULL;
-    char erro;
-    list = get_list();
-    while (strcmp(list[0], "exit") && strcmp(list[0], "quit")) {
-        erro = perform(list);
-        if (erro) {
-            break;
+            if (execvp(list[0], list) < 0) {
+                perror("execvp failed!\n");
+                return;
+            }
         }
         delete_list(list);
         list = get_list();
     }
     delete_list(list);
+    return;
+}
+
+int main() {
+    char **list = NULL, flag;
+    list = get_list();
+    infinit(list);
     return 0;
 }
